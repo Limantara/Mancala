48bcf318011707949da44f688157f22a14f195c7
fantasysukun <188454747@qq.com>
2015-05-03 18:25:49 -0700

    Game logic fixed

--- Board.java
+++ Board.java
@@ -275,22 +275,26 @@ public class Board
 			//If it is A's turn, and he click on pit[5]
 			if(pit == PitPanel.LAST_LOWER_PIT && remainingStones > 0 && whosePit == Board.PLAYER_A)
 			{
+				System.out.println("1 pit: " + pit + " <- execpted 5");
 				stones[Board.MANCALA_A_HOLE]++;
 				MancalaAdded = true;
 				//set playerA for next turn when last pit in mancala 
 				if(pit == PitPanel.LAST_LOWER_PIT && remainingStones == 1 && whosePit == Board.PLAYER_A)
 				{
-					//System.out.println("pit A: " + pit);
+					//System.out.println("pit A: " + pit + " <- execpted 5");
+					System.out.println("2 pit: " + pit + " <- execpted 5");
 					playerA.setMyTurn(true);
 					playerB.setMyTurn(false);
 				}
 				
 				remainingStones--;
 				pit = PitPanel.FIRST_UPPER_PIT;
+				System.out.println("3 pit: " + pit + " <- execpted 6");
 			}
 			//If it is B's turn, and he click on pit[11]
 			else if(pit == PitPanel.LAST_UPPER_PIT && remainingStones > 0 && whosePit == Board.PLAYER_B)
 			{
+				
 				stones[Board.MANCALA_B_HOLE]++;
 				MancalaAdded = true;
 				//set playerB for next turn when last pit in mancala 
@@ -304,35 +308,39 @@ public class Board
 				remainingStones--;
 				pit = PitPanel.FIRST_LOWER_PIT;
 			}		
-			else
+			
+			//boolean continues = true;
+			else if(MancalaAdded) //if Mancala added, the pit is already added so do nothing
 			{
-				if(MancalaAdded) //if Mancala added, the pit is already added so do nothing
-				{
-					//do nothing
-					MancalaAdded = false; //set MancalaAdded back to default
-					//System.out.println("do nothing");
-				}
-				else
-				{
-					pit++; 
-					
-				}
-				
-				//System.out.println("pit+1: " + pit);
+				//do nothing
+				System.out.println("4 pit: " + pit + " <- execpted 0 or 6");
 				stones[pit]++;
 				remainingStones--;
-				
+				MancalaAdded = false; //set MancalaAdded back to default
+				//continues = false;
+				//System.out.println("do nothing");
+			}
+			else
+			{
+				//System.out.println("4 pit: " + pit + " <- execpted 5");
+				pit++; 
+				stones[pit]++;
+				remainingStones--;
+				//System.out.println("5 pit: " + pit + " <- execpted 6");
+				//continues = false;
 			}
 			
 			//If the last stone you drop is in an empty pit on your side, 
 			//you get to take that stone and all of your opponents stones that are in the opposite pit.
-			if(stones[pit] == 1 && remainingStones == 0 && pit >= PitPanel.FIRST_LOWER_PIT && pit <= PitPanel.FIRST_UPPER_PIT &&  whosePit == Board.PLAYER_A)
+			if(stones[pit] == 1 && remainingStones == 0 && pit >= PitPanel.FIRST_LOWER_PIT && pit <= PitPanel.LAST_LOWER_PIT &&  whosePit == Board.PLAYER_A)
 			{
+				//System.out.println("pit: " + pit + " whosepit: " + whosePit);
 				stones[pit] += stones[11-pit];
 				stones[11-pit] = 0;
 			}
-			else if(stones[pit] == 1 && remainingStones == 0 && pit >= PitPanel.LAST_LOWER_PIT && pit <= PitPanel.LAST_UPPER_PIT &&  whosePit == Board.PLAYER_B)
+			else if(stones[pit] == 1 && remainingStones == 0 && pit >= PitPanel.FIRST_UPPER_PIT && pit <= PitPanel.LAST_UPPER_PIT &&  whosePit == Board.PLAYER_B)
 			{
+				//System.out.println("pit: " + pit + " whosepit: " + whosePit);
 				stones[pit] += stones[11-pit];
 				stones[11-pit] = 0;
 			}
