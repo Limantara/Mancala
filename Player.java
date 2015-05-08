7818bc545cb3152e70b6e4814660ae10d1428214
Limantara <edwin@limantara.me>
2015-05-08 15:53:20 -0700

    Add proper javadocs

--- Player.java
+++ Player.java
@@ -7,30 +7,45 @@
  */
 
 /**
- * this is a class for player rules 
+ * The model of the player containing his/ her information.
  * @author Team Architects
  */
 public class Player 
 {
-	private boolean myTurn;
-	
+	/**
+	 * Construct a new player object.
+	 */
 	public Player() 
 	{
 		this(false);
 	}
 	
+	/**
+	 * Construct a new player object and set if it is his turn.
+	 * @param  myTurn true to set this player to move first or false otherwise
+	 */
 	public Player(boolean myTurn)
 	{
 		this.myTurn = myTurn;
 	}
 	
+	/**
+	 * Check if this is this player's turn.
+	 * @return true if this is this player's turn or false otherwise.
+	 */
 	public boolean isMyTurn()
 	{
 		return myTurn;
 	}
 	
+	/**
+	 * Set this player turn.
+	 * @param myTurn true to set this player to move next or false otherwise.
+	 */
 	public void setMyTurn(boolean myTurn)
 	{
 		this.myTurn = myTurn;
 	}
+
+	private boolean myTurn;
 }
